2a6ea92721aea54d2453539b6cb6b3bc8209db1d
Clara-assistante <csi-entreprise@hotmail.fr>
2021-05-02 16:56:50 +0200

    Add files via upload

---
+++ vendredi_13.js
@@ -0,0 +1,45 @@
+exports.action = function(data){
+var moment = require('moment');moment.locale('fr'); 
+    	
+JarvisAskMe('Okay... mais pour quelle année...',function(result){
+    console.log("Recherche les vendredi 13 de : "+result.trim())
+        var result=result
+
+    var mois = new Array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre");
+    var nbr = new Array("1", "2", "3")
+
+    function vendredi (result) {
+
+        vdd = 0; //OPTIONEL contient le nombre de vendredi 13, comme il y en a maximum 3 par années (merci google) elle servira a transformer le nombre en texte avec l'array plus haut
+        cetteannee = result; //OPTIONEL récupère l'année pour l'afficher dans rep
+        annee = new Date(+result, 0); //récupère l'année en la mettant dans une variable date, le mois est mit à 0 car on est obligé d'indiquer au mois une année et un mois
+        auj = new Date(); //OPTIONEL date du jour pour le temps du verbe
+
+        rep = "";   //contient la chaine de caractères qui sera affiché
+        s = "";     //OPTIONEL contient le pluriel
+        x = "";     //OPTIONEL contient le pluriel
+        temps = ""; //OPTIONEL contient le temps du verbe (passé présent ou futur)
+
+        for (i = 0; i < 12; i++) { annee.setMonth(i, 13);
+            if (annee.getDay() == 5) { 
+                rep = rep + " le vendredi 13 "+mois[i]+",";
+                vdd = vdd + 1; }
+                 }
+        
+        if (vdd != 1) { s = "s"; x = "x"; }
+
+        if (annee.getFullYear() < auj.getFullYear()) { temps = "a eu"; }
+        if (annee.getFullYear() > auj.getFullYear()) { temps = "aura"; }
+        if (annee.getFullYear() == auj.getFullYear()) { temps = "a"; }
+
+        vdd = nbr[vdd - 1];
+
+        rep = "En " + cetteannee + ", il y en " + temps + " " + vdd + ":" + rep;
+       console.log(rep);
+        JarvisIASpeech(rep);
+    }
+
+
+vendredi(result);
+})//fin ask
+}
